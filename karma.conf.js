1f557c7286b9c3f38cc5fbe54121977a9c93ca4a
willmendesneto <willmendesneto@gmail.com>
2015-03-01 17:40:47 -0300

    chore: add code coverage

--- karma.conf.js
+++ karma.conf.js
@@ -59,7 +59,14 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress',  'coverage'],
+
+
+    //  coverage reporter configuration
+    coverageReporter: {
+      type:'html',
+      dir:'coverage'
+    },
 
 
     // web server port
