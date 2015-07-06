f58b5addd9130c6de49fa94f80e9b1dc74ecba9c
Ciro Nunes <ciroanunes@gmail.com>
2015-07-06 20:47:55 -0300

    refactor(controllers): fix the destroy tests
    100% coverage

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -4,11 +4,10 @@
   angular.module('myApp')
     .controller('SampleController', SampleController);
 
-  function SampleController($scope) {
+  function SampleController($scope, sampleService) {
     var vm = this;
 
     vm.bar = bar;
-    vm.doSomething = doSomething;
     vm.sendMessage = sendMessage;
     vm.broadcastEvent = broadcastEvent;
     vm.foo = 'bar';
@@ -16,8 +15,6 @@
     function bar() {
       vm.foo = 'baz';
     }
-
-    function doSomething() {}
 
     function sendMessage() {
       $scope.$emit('sample:message', { foo: 'bar' });
@@ -34,10 +31,14 @@
     });
 
     $scope.$on('$destroy', function() {
-      vm.doSomething();
+      sampleService.bar();
     });
 
     //scope.$on()
     //scope.$apply()
   }
+
+  SampleController.$inject = ['$scope', 'sampleService'];
 }());
+
+
--- controllers/controllers.spec.js
+++ controllers/controllers.spec.js
@@ -1,12 +1,13 @@
 describe('SampleController', function() {
   var $rootScope,
-    ctrl, scope;
+    ctrl, scope, sampleService;
 
   beforeEach(module('myApp'));
 
-  beforeEach(inject(function( _$rootScope_, _$controller_) {
+  beforeEach(inject(function( _$rootScope_, _$controller_, _sampleService_) {
     $rootScope = _$rootScope_;
     scope = $rootScope.$new();
+    sampleService = _sampleService_;
 
     ctrl = _$controller_('SampleController', {
       $scope: scope
@@ -23,15 +24,16 @@ describe('SampleController', function() {
   });
 
   it('should have a watcher', function() {
+    scope.$digest(); // run once before change a value just to make the coverage 100%
     scope.baz = 'foo';
     scope.$digest();
     expect(ctrl.baz).toBe('foobar');
   });
 
   it('should do something on $destroy', function() {
-    spyOn(ctrl, 'doSomething');
+    spyOn(sampleService, 'bar');
     scope.$destroy();
-    expect(ctrl.doSomething).toHaveBeenCalled();
+    expect(sampleService.bar).toHaveBeenCalled();
   });
 
   it('should emit an event', function() {
